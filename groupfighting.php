2355f44132e8371416ed81330f8ccb04d908dd99
Jonathan Allen <jallen@uccs.edu>
2017-08-20 05:48:16 -0600

    removed old code

--- groupfighting.php
+++ groupfighting.php
@@ -196,46 +196,6 @@
         </ul>
       </nav>
     </section>
-    <!-- Section Two
-    <section class="one_quarter">
-      <h2 class="title">Link Block</h2>
-      <nav>
-        <ul>
-          <li><a href="#">Lorem ipsum dolor sit</a></li>
-          <li><a href="#">Amet consectetur</a></li>
-          <li><a href="#">Praesent vel sem id</a></li>
-          <li><a href="#">Curabitur hendrerit est</a></li>
-          <li class="last"><a href="#">Sed a nulla urna</a></li>
-        </ul>
-      </nav>
-    </section> -->
-    <!-- Section Three
-    <section class="one_quarter">
-      <h2 class="title">Link Block</h2>
-      <nav>
-        <ul>
-          <li><a href="#">Lorem ipsum dolor sit</a></li>
-          <li><a href="#">Amet consectetur</a></li>
-          <li><a href="#">Praesent vel sem id</a></li>
-          <li><a href="#">Curabitur hendrerit est</a></li>
-          <li class="last"><a href="#">Sed a nulla urna</a></li>
-        </ul>
-      </nav>
-    </section>-->
-    <!-- Section Four
-    <section class="one_quarter lastbox">
-      <h2 class="title">Link Block</h2>
-      <nav>
-        <ul>
-          <li><a href="#">Lorem ipsum dolor sit</a></li>
-          <li><a href="#">Amet consectetur</a></li>
-          <li><a href="#">Praesent vel sem id</a></li>
-          <li><a href="#">Curabitur hendrerit est</a></li>
-          <li class="last"><a href="#">Sed a nulla urna</a></li>
-        </ul>
-      </nav>
-    </section> -->
-    <!-- / Section -->
   </div>
 </div>
 <!-- Copyright -->
